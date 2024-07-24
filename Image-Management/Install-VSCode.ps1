e4bb52659657b9e5fbe4544838bb81b2cd57b8a1
Asithdee <126492671+Asithdee@users.noreply.github.com>
2024-07-23 22:59:07 -0400

    Update Install-VSCode.ps1

--- Image-Management/Install-VSCode.ps1
+++ Image-Management/Install-VSCode.ps1
@@ -1,1 +1,12 @@
-test
+Try {
+       
+    # Download VSCode Sources
+    Write-Host "Downloading VS Code sources..."
+    Invoke-WebRequest -Uri 'https://aka.ms/vscode-win32-x64-system-stable' -OutFile 'c:\windows\temp\VSCode_x64.exe'
+    # Wait 10s
+    Start-Sleep -Seconds 10
+    # Install VSCode silently
+    Write-Host "Installing VS Code now..."
+    Start-Process -FilePath 'c:\windows\temp\VSCode_x64.exe' -Args '/verysilent /suppressmsgboxes /mergetasks=!runcode' -Wait -PassThru
+    Write-Host "Successfully installed VS Code..."
+}
