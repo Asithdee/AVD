3f8fc32a9afe5a2ac7520300bcc7acb63cfc4ebc
Asitha De Silva <126492671+Asithdee@users.noreply.github.com>
2025-01-22 09:43:02 -0500

    Create PrerequisitesAzureVM.ps1

---
+++ ARC/PrerequisitesAzureVM.ps1
@@ -0,0 +1,7 @@
+#Azure VM to ARC
+[System.Environment]::SetEnvironmentVariable("MSFT_ARC_TEST",'true', [System.EnvironmentVariableTarget]::Machine)
+ 
+Set-Service WindowsAzureGuestAgent -StartupType Disabled -Verbose
+Stop-Service WindowsAzureGuestAgent -Force -Verbose
+ 
+New-NetFirewallRule -Name BlockAzureIMDS -DisplayName "Block access to Azure IMDS" -Enabled True -Profile Any -Direction Outbound -Action Block -RemoteAddress 169.254.169.254
